9a8b2aca40e27d7467905d64cb451eeff371c7ef
Enzo <nuneslopesenzo02@gmail.com>
2026-08-20 17:01:36 -0300

    feat codificando a classe bike e propriedades e métodos

--- Programa1/Bike.cs
+++ Programa1/Bike.cs
@@ -11,6 +11,32 @@
 
         public int RecargaMes { get; set; }
 
+        //Métodos(Ações)
+
+        public double CalcularAutonomia()
+        {
+            double autonomiaBase = Bateria / 15.0;
+            bool peso = PesoCiclista > 90;
+            //Se o peso do ciclista for maior que 90 quilos reduza a autônomia
+            //para 15% senão autonomia normal
+            return peso ? (autonomiaBase * 0.05) : autonomiaBase;
+        }
+
+        public double CalcularConsumoMenasal()
+        {
+            double CargaPorKM = Bateria / 1000.0;
+            return CargaPorKM * RecargaMes;
+        }
+
+        public double CalcularCustoMensal()
+        {
+            const double PRECO = 0.80;
+            return CalcularConsumoMenasal() * PRECO;
+        }
+
+        public bool BikeEconomica()
+        {
+            return (CalcularCustoMensal() < 15.00) || (CalcularAutonomia() > 40);
+        }
    }
 }
-4
